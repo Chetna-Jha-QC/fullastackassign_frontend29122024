99ac4d8d673a304a4db7441aaa9e7881847a88f1
Chetna-Jha-QC <ms.chetnajha@gmail.com>
2024-12-29 19:33:01 +0530

    login.js

--- scripts/login.js
+++ scripts/login.js
@@ -3,14 +3,14 @@ document.getElementById('login-form').addEventListener('submit', async (e) => {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    const response = await fetch('https://steadfast-lily-toy.glitch.me');
+    const response = await fetch('https://steadfast-lily-toy.glitch.me/users');
     const users = await response.json();
 
     const user = users.find(user => user.email === email && user.password === password);
 
     if (user) {
         localStorage.setItem('user', JSON.stringify(user));
-        alert('Login successful!');
+        alert('Login successful.');
         window.location.href = './todos.html';
     } else {
         alert('Invalid credentials.');
